950844e19291ae3064bb30641e87c90dc98ddbfe
Heorgii <103379960+Heorgii@users.noreply.github.com>
2022-09-14 18:49:54 +0000

    Deploying to gh-pages from @ Heorgii/goit-js-hw-09@34d0ef65026eb900fa4ef3ffba3c94ffa6671962 🚀

---
+++ 02-timer.9e89b173.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]");document.querySelector("[data-days]"),document.querySelector("[data-hours]"),document.querySelector("[data-minutes]"),document.querySelector("[ data-seconds]");e.setAttribute("disabled",!0);new Date;
+//# sourceMappingURL=02-timer.9e89b173.js.map
---
+++ 02-timer.fc2661c9.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]");document.querySelector("[data-days]"),document.querySelector("[data-hours]"),document.querySelector("[data-minutes]"),document.querySelector("[ data-seconds]");e.setAttribute("disabled",!0);new Date}();
+//# sourceMappingURL=02-timer.fc2661c9.js.map
