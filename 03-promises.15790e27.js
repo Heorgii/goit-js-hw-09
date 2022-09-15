e5de2fab0f57b90b60418c4c7e3f787a099ffb2b
Heorgii <103379960+Heorgii@users.noreply.github.com>
2022-09-15 17:14:13 +0000

    Deploying to gh-pages from @ Heorgii/goit-js-hw-09@c6c7c918c2a501a12efcdbb25feedf34d69dd12b 🚀

---
+++ 03-promises.15790e27.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),document.querySelector("button");(void Math.random()).then((({position:e,delay:o})=>{Notify.failure(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{Notify.failure(`❌ Rejected promise ${e} in ${o}ms`)}));
+//# sourceMappingURL=03-promises.15790e27.js.map
---
+++ 03-promises.b933a7f3.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".form"),document.querySelector("button");(void Math.random()).then((function(o){var t=o.position,e=o.delay;Notify.failure("✅ Fulfilled promise ".concat(t," in ").concat(e,"ms"))})).catch((function(o){var t=o.position,e=o.delay;Notify.failure("❌ Rejected promise ".concat(t," in ").concat(e,"ms"))}))}();
+//# sourceMappingURL=03-promises.b933a7f3.js.map
