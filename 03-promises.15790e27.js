2cf0ae3b418d99bacd4bf327ba78260283802e60
Heorgii <103379960+Heorgii@users.noreply.github.com>
2022-09-17 12:37:21 +0000

    Deploying to gh-pages from @ Heorgii/goit-js-hw-09@aafd1f53ad7358830fbc49c4ae434effd2ef552d 🚀

--- 03-promises.15790e27.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form"),document.querySelector("button");(void Math.random()).then((({position:e,delay:o})=>{Notify.failure(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{Notify.failure(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.15790e27.js.map
--- 03-promises.b933a7f3.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".form"),document.querySelector("button");(void Math.random()).then((function(o){var t=o.position,e=o.delay;Notify.failure("✅ Fulfilled promise ".concat(t," in ").concat(e,"ms"))})).catch((function(o){var t=o.position,e=o.delay;Notify.failure("❌ Rejected promise ".concat(t," in ").concat(e,"ms"))}))}();
-//# sourceMappingURL=03-promises.b933a7f3.js.map
